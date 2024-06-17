e21bd885aca3045380e9ebfb953b03bf031f4343
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-06-17 19:03:36 +0100

    Fix other profiles accessibility bug

--- app/profiles/views.py
+++ app/profiles/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, session, redirect, url_for
-from flask_login import login_required
+from flask import Blueprint, render_template, session, redirect, url_for, flash
+from flask_login import login_required, current_user
 from app.auth.models import User
 
 profiles_bp = Blueprint('profiles', __name__, template_folder='../templates')
@@ -7,7 +7,9 @@ profiles_bp = Blueprint('profiles', __name__, template_folder='../templates')
 @profiles_bp.route("/profile/<username>")
 @login_required
 def profile(username):
-    username = User.find_by_username(username).username
+    if current_user.username != username:
+        flash("You are not authorized to view this profile.")
+        return redirect(url_for('profiles.profile', username=current_user.username))
 
     return render_template("profile.html", username=username)
     
