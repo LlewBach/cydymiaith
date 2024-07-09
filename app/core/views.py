eacc7dbcbc010b21ce7120e1c23d6517f1ad5182
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-07-09 19:03:46 +0100

    Add docstring to core views

--- app/core/views.py
+++ app/core/views.py
@@ -6,4 +6,10 @@ core_bp = Blueprint('core', __name__, template_folder='../templates')
 @core_bp.route("/")
 @core_bp.route("/home")
 def home():
+    """
+    Renders the home page template.
+
+    Returns:
+        Response: The rendered "home.html" template.
+    """
     return render_template("home.html")
