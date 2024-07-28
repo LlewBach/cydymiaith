b1c6733a790f14e1cb5bc9f92cd9e97b5a57fbb4
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-07-28 18:11:06 +0100

    Lint core views and models python

--- app/core/models.py
+++ app/core/models.py
@@ -6,13 +6,16 @@ class Core:
         """
         Retrieves the list of categories from the database.
 
-        This method queries the database to retrieve all category documents from the categories collection and returns them as a list.
+        This method queries the database to retrieve all category documents
+        from the categories collection and returns them as a list.
 
         Returns:
-            list: A list of category documents from the categories collection. If an exception occurs, returns an empty list.
+            list: A list of category documents from the categories collection.
+            If an exception occurs, returns an empty list.
 
         Raises:
-            Exception: If there is an issue with the database query, the exception is caught and an error message is printed.
+            Exception: If there is an issue with the database query, the
+            exception is caught and an error message is printed.
         """
         try:
             categories = list(mongo.db.categories.find())
@@ -20,7 +23,6 @@ class Core:
         except Exception as e:
             print(f"Error in get_categories method: {e}")
             return []
-        
 
     @staticmethod
     def get_levels():
@@ -31,7 +33,6 @@ class Core:
             list: A list of level documents from the levels collection.
         """
         return list(mongo.db.levels.find())
-    
 
     @staticmethod
     def get_providers():
@@ -42,15 +43,14 @@ class Core:
             list: A list of provider documents from the providers collection.
         """
         return list(mongo.db.providers.find())
-    
 
     @staticmethod
     def get_roles():
         """
         Retrieves a list of all roles from the database.
 
-        This static method queries the database for all documents in the 'roles' collection
-        and returns them as a list.
+        This static method queries the database for all documents in the
+        'roles' collection and returns them as a list.
 
         Returns:
             list: A list of all roles from the database.
