31c3058632b788045cba2692c56947a26f301a2b
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-07-28 19:32:14 +0100

    Lint app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ mongo = PyMongo()
 login_manager = LoginManager()
 mail = Mail()
 
+
 def create_app():
     app = Flask(__name__)
     # MongoDB Configuration
@@ -21,14 +22,16 @@ def create_app():
     # Flask-Mail Configuration
     app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.example.com')
     app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
-    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'true').lower() in ['true', '1', 't']
+    app.config['MAIL_USE_TLS'] = os.getenv(
+        'MAIL_USE_TLS', 'true').lower() in ['true', '1', 't']
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-    app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'noreply@example.com')
+    app.config['MAIL_DEFAULT_SENDER'] = os.getenv(
+        'MAIL_DEFAULT_SENDER', 'noreply@example.com')
 
     # Initializing Extensions
     mongo.init_app(app)
-    login_manager.init_app(app) #
+    login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
     mail.init_app(app)
 
