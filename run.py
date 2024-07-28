ca2faea2c285300d2f9dc85413c1b385ea6523db
LlewBach <37737969+LlewBach@users.noreply.github.com>
2024-07-28 19:36:38 +0100

    Lint run.py

--- run.py
+++ run.py
@@ -4,4 +4,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)
